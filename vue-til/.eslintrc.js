3eff23cf139491e54363a8953743472fee8d88d2
cre4tive97 <harryborrison5148@gmail.com>
2021-10-28 23:32:56 +0900

    ESLint, Prettier setting

--- vue-til/.eslintrc.js
+++ vue-til/.eslintrc.js
@@ -1,3 +1,5 @@
+const { off } = require("process");
+
 module.exports = {
   root: true,
   env: {
@@ -10,6 +12,16 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier" : ['error', {
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      }]
   },
   overrides: [
     {
