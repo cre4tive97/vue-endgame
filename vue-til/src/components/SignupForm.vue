9ead65d002c8e3bafc6d527909aae8e775642282
cre4tive97 <harryborrison5148@gmail.com>
2021-10-30 13:52:10 +0900

    SignupForm markup

---
+++ vue-til/src/components/SignupForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <div>
+      <label for="username">ID : </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">PW : </label>
+      <input id="password" type="password" />
+    </div>
+    <div>
+      <label for="nickname">Nickname : </label>
+      <input id="nickname" type="text" />
+    </div>
+    <button type="submit">Sign Up</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SignupForm',
+};
+</script>
+
+<style></style>
