40be2e3a119815de45274d8cb3efa27a5ad3d2f4
cre4tive97 <harryborrison5148@gmail.com>
2021-10-30 17:45:07 +0900

    LoginForm markup

---
+++ vue-til/src/components/common/LoginForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <div>
+      <label for="username"></label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password"></label>
+      <input id="password" type="text" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
