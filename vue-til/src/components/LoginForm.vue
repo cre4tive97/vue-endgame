5a731bcc207e8ca3b73853072183d0a104b1e0a8
cre4tive97 <harryborrison5148@gmail.com>
2021-10-30 21:15:12 +0900

    async await의 에러처리

---
+++ vue-til/src/components/LoginForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username"></label>
+      <input id="username" v-model="username" type="text" />
+    </div>
+    <div>
+      <label for="password"></label>
+      <input id="password" v-model="password" type="text" />
+    </div>
+    <button type="submit">로그인</button>
+    <p>{{ logMessage }}</p>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api/index';
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      logMessage: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const userData = { username: this.username, password: this.password };
+        const { data } = await loginUser(userData);
+        this.logMessage = `${data.user.username} 님 환영합니다.`;
+        this.initForm();
+      } catch (error) {
+        this.logMessage = error.response.data;
+      }
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+    },
+  },
+};
+</script>
+
+<style></style>
